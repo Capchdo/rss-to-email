c4f57c9d451a2eed10e5dd4cb2ffe201dbbe3e3b
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2023-12-07 11:48:10 +0800

    Update feeds.ts

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,10 +1,3 @@
 export const feeds = [
-  // Daring Fireball
-  'https://daringfireball.net/feeds/main',
-
-  // JakeArchibald.com
-  'https://jakearchibald.com/posts.rss',
-
-  // Appjeniksaan
-  'https://appjeniksaan.nl/feed.xml',
+  'https://haobit.top/dev/feed.rss',
 ]
